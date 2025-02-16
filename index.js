c250b46d73a56737508ec96556d979e06633da8d
cowflipper <hoff3454@gmail.com>
2025-02-16 17:21:16 +0300

    last1

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const options = document.querySelectorAll('.option');
 const desc = document.querySelectorAll('.desc_opt');
 
 
+
+
+// for(let i = 0; i < options.length; i++) {
+//     options[i].addEventListener('click', function() {
+//             desc[i].classList.remove('desc_opt1')
+//             desc[i].classList.add('desc_opt')
+//     })
+// }
+
 for(let i = 0; i < options.length; i++){
-    options[i].addEventListener('mouseenter', function(){
+    options[i].addEventListener('click', function(){
         for(let i = 0; i < options.length; i++) {
             desc[i].classList.remove('desc_opt1')
         } 
         desc[i].classList.toggle('desc_opt1')
-    })
-}
-
-for(let i = 0; i < options.length; i++) {
-    options[i].addEventListener('mouseleave', function() {
-            desc[i].classList.remove('desc_opt1')
-            desc[i].classList.add('desc_opt')
     })
 }
